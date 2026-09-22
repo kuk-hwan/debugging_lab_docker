ec01399cae862b6aa4a46ed5f1ba7aec2441bb86
kuk-hwan <xi.crebig@gmail.com>
2026-09-22 07:04:49 +0000

    heap_buffer_overflow

--- challenges/03_heap_buffer_overflow/bug.c
+++ challenges/03_heap_buffer_overflow/bug.c
@@ -63,9 +63,14 @@ static void list_ensure(IntList *l, size_t need) {
     if (need <= l->cap) return;
 
     size_t newcap = l->cap ? l->cap * 2 : 8;
-    while (newcap < need) newcap *= 2;
 
-    int *p = realloc(l->data, l->cap * sizeof(int));
+    while (newcap < need) newcap *= 2;
+    
+    int *p = realloc(
+        l->data,
+        newcap * sizeof(int)
+    );
+
     if (!p) { perror("realloc"); free(l->data); exit(1); }
 
     l->data = p;
